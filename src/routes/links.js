9454aa23c532fc81ec8711461d680c5dc2f6b997
Ramiro Mejías Rodríguez <ramiro.mejias@gmail.com>
2023-11-27 20:08:23 -0500

    Some routes created

---
+++ src/routes/links.js
@@ -0,0 +1,10 @@
+const express = require("express");
+const router = express.Router();
+
+const pool = require("../database");
+
+router.get("/add", (req, res) => {
+  res.render("links/add");
+});
+
+module.exports = router;
